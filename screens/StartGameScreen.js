2c68bb16a9315e8747182e987ebc58232beb92e0
Guxim <guxim.selmani1@hotmail.com>
2021-07-06 02:25:56 +0200

    Number Guess.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,32 +1,89 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import Card from "../components/Card";
 const StartGameScreen = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+  const numberInputHandler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, "")); // deletes every character that is not number
+  };
+  const resetInputHandler = () => {
+    setEnteredValue("");
+    setConfirmed(false);
+  };
+  const confirmedInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+      return;
+    }
+
+    setConfirmed(true);
+    setSelectedNumber(chosenNumber);
+    setEnteredValue("");
+  };
+  let confirmedOutput;
+  if (confirmed) {
+    confirmedOutput = <Text>Chosen number is:{" " + selectedNumber}</Text>;
+  }
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.myTitle}>Start a New Game</Text>
-      <View style={styles.inputContainer}>
-        <Text>Select a Number</Text>
-        <TextInput style={styles.myTextInput} />
-        <View style={styles.buttonContainer}>
-          <Button title="  Reset  " onPress={() => {}} />
-          <Button title="Confirm" onPress={() => {}} />
-        </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss(); // dismisses the keyboard if u tap in the screen
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.myTitle}>Start a New Game</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a Number</Text>
+          <TextInput
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false} // doesn't autocorrect when u put numbers in the text input
+            keyboardType="number-pad" // keyobard has only numbers
+            maxLength={2} // the length of the text is set to 2 numbers
+            style={styles.myTextInput}
+            onChangeText={numberInputHandler}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <Button
+              title="   Reset   "
+              onPress={resetInputHandler}
+              color="#c717fc"
+            />
+            <Button
+              title="Confirm"
+              onPress={confirmedInputHandler}
+              color="red"
+            />
+          </View>
+        </Card>
+        {confirmedOutput}
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 10,
+    padding: 5,
     alignItems: "center",
   },
   myTextInput: {
-    borderWidth: 1,
-    width: 300,
+    borderBottomWidth: 1,
+    width: 50,
     maxWidth: "80%",
-    height: 40,
     marginVertical: 10,
     textAlign: "center",
   },
@@ -38,14 +95,6 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    shadowColor: "black",
-    shadowOffset: { width: 2, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    elevation: 10,
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: "row",
